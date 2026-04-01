4ca1a37e0fab7decc40f627a8db33c34b7c99217
Lee <264464762+leehart3000@users.noreply.github.com>
2026-04-01 10:27:06 -0400

    WIP: dev

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "isbn": "978-0141393964", "reviews": {} },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "isbn": "978-1840228625", "reviews": {} },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "isbn": "978-1857151831", "reviews": {} },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "isbn": "978-0141907185", "reviews": {} },
+      5: {"author": "Unknown","title": "The Book Of Job", "isbn": "978-3849677497", "reviews": {} },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "isbn": "978-1408827765", "reviews": {} },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "isbn": "978-0140441031", "reviews": {} },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "isbn": "978-0141439518", "reviews": {} },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "isbn": "979-8313011103", "reviews": {} },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "isbn": "978-0714541075", "reviews": {} }
 }
 
 module.exports=books;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,32 +12,59 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+	// Send JSON response with formatted books data
+    res.send(JSON.stringify(books,null,4));
+
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+    // Retrieve the ISBN parameter from the request URL and send the corresponding book details
+    const isbn = req.params.isbn;
+    const booksByISBN = Object.values(books).filter(book => book.isbn === isbn);
+
+	// Send JSON response with formatted booksByISBN data
+    res.send(JSON.stringify(booksByISBN,null,4));
+
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+    // Retrieve the author parameter from the request URL and send the corresponding book details
+    const author = req.params.author;
+    const booksByAuthor = Object.values(books).filter(book => book.author === author);
+
+	// Send JSON response with formatted booksByAuthor data
+    res.send(JSON.stringify(booksByAuthor,null,4));
+
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+    // Retrieve the title parameter from the request URL and send the corresponding book details
+    const title = req.params.title;
+    const booksByTitle = Object.values(books).filter(book => book.title === title);
+
+	// Send JSON response with formatted booksByTitle data
+    res.send(JSON.stringify(booksByTitle,null,4));
+
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+    // Retrieve the ISBN parameter from the request URL and send the corresponding book reviews
+    const isbn = req.params.isbn;
+    const booksByISBN = Object.values(books).filter(book => book.isbn === isbn);
+    const bookReviews = booksByISBN.map(book => book.reviews)
+
+	// Send JSON response with formatted bookReviews data
+    res.send(JSON.stringify(bookReviews,null,4));
+
 });
 
 module.exports.general = public_users;
