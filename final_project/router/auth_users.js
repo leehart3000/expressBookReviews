d22e05b83b18091aac3c78ad182254f9d82497e7
Lee <264464762+leehart3000@users.noreply.github.com>
2026-04-01 11:27:52 -0400

    WIP: dev

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,17 +6,37 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+    //write code to check is the username is valid
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+
+    // Check if username and password match the one we have in records.
+
+    // Filter the users array for any user with the same username and password
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    // Return true if any valid user is found, otherwise false
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
+
+
+    
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
+
+
+
+
 });
 
 // Add a book review
